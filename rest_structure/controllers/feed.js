2c8af59a58492f07de6d6589ae0d6671115090e3
Ahsan <ahsanfarooq3423@gmail.com>
2020-03-13 16:52:07 +0500

    fethcing operational

--- rest_structure/controllers/feed.js
+++ rest_structure/controllers/feed.js
@@ -1,6 +1,15 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts : [{title : 'my first post', content : 'this is my first post!!'}]
+        posts: [{
+            _id : '1',
+            title: 'this is the first post in json',
+            content: 'this is my first post!!',
+            imageUrl: 'images/duck.jpg',
+            creator : {
+                name : 'Ahsan'
+            },
+            createdAt : new Date()
+        }]
     })
 }
 
@@ -8,7 +17,9 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
     res.status(201).json({
-        message : 'Post Created Successfully!',
-        post : {id : new Date().toISOString() , title, content}
+        message: 'Post Created Successfully!',
+        post: { id: new Date().toISOString(), title, content }
     })
 }
+
+
