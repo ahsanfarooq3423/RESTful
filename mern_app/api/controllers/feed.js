0739383265670ad750dbf9a5ba33bec38791cbae
Ahsan <ahsanfarooq3423@gmail.com>
2020-03-23 19:48:41 +0500

    mern rest complete

--- mern_app/api/controllers/feed.js
+++ mern_app/api/controllers/feed.js
@@ -186,6 +186,14 @@ exports.deletePost = (req, res, next) => {
             return Post.findByIdAndDelete(postId);
         })
         .then(result => {
+            return User.findById(req.userId);
+            
+        })
+        .then(user => {
+            user.posts.pull(postId);
+            return user.save()
+        })
+        .then(result => {
             res.status(200).json({
                 message: 'Post Deleted.'
             })
